a314c943ce42ce876796c804b3f2b6c59761f554
MINJU KIM <yd5726@naver.com>
2022-10-08 02:47:07 +0900

    update join, create test
    update join, create test

---
+++ javascript/joinS.js
@@ -0,0 +1,7 @@
+function togglecheckboxes(mas,group){
+	var cbarray = document.getElementsByClassName(group);
+	for(var i = 0; i < cbarray.length; i++){
+		var cb = document.getElementById(cbarray[i].id);
+		cb.checked = mas.checked;
+	}
+}

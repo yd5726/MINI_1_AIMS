21a816dc3eaf52ba30972b5f66283bef7992992b
MINJU KIM <yd5726@naver.com>
2022-10-09 02:48:16 +0900

    update joinS, joinT
    update joinS, joinT

--- javascript/joinS.js
+++ javascript/joinS.js
@@ -1,7 +1,28 @@
+/*전체 선택*/
 function togglecheckboxes(mas,group){
 	var cbarray = document.getElementsByClassName(group);
 	for(var i = 0; i < cbarray.length; i++){
 		var cb = document.getElementById(cbarray[i].id);
 		cb.checked = mas.checked;
+	}
+}
+/*사진 업로드*/
+function setIDpicture(event) {
+	var reader = new FileReader();
+
+	reader.onload = function(event) {
+	  var img = document.createElement("img");
+	  img.setAttribute("src", event.target.result);
+	  document.querySelector("div#ID-picture").appendChild(img);
+	};
+	reader.readAsDataURL(event.target.files[0]);
+}
+
+/*input[type=text]특정 타입 입력 막기*/
+function characterCheck(obj){
+	var regExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/; 
+	if( regExp.test(obj.value) ){
+		alert("특수문자는 입력하실수 없습니다.");
+		obj.value = obj.value.substring(0, obj.value.length - 1);
 	}
 }
